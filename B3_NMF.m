3a25659ec069a532aa9b27ce5abfcc72801469b6
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2016-05-06 12:52:54 +0800

    New pic

--- B3_NMF.m
+++ B3_NMF.m
@@ -15,10 +15,35 @@ inW = rand([shape(1),3]);
 inH = rand(3 , shape(2));
 [W,H] = nmf(m,inW,inH,0.00001,2,1000);
 
+par.logComp = 100;
+par.fAxis = f;
+
+visualizeSpec([W, sum(m')'/100],par);
+par.tAxis = t;
+
+
 rm = W*H;
 rY = rm.*cos(angl) + i*rm.*sin(angl);
-visualizeSpec(rY);
+visualizeSpec(rY,par);
 
 fst = W(:,1)*H(1,:);
 fst = fst.*cos(angl) + i*fst.*sin(angl);
-visualizeSpec(fst);
+visualizeSpec(fst,par);
+
+snd = W(:,2)*H(2,:);
+snd = snd.*cos(angl) + i*snd.*sin(angl);
+visualizeSpec(snd,par);
+
+trd = W(:,3)*H(3,:);
+trd = trd.*cos(angl) + i*trd.*sin(angl);
+visualizeSpec(trd,par);
+
+fst =  istft(fst, h, nfft, fs);
+figure;
+spPitchCorr(spCorr(fst, fs, [],'plot'), fs)
+snd =  istft(snd, h, nfft, fs);
+figure;
+spPitchCorr(spCorr(snd, fs, [],'plot'), fs)
+trd =  istft(trd, h, nfft, fs);
+figure;
+spPitchCorr(spCorr(trd, fs, [],'plot'), fs)
